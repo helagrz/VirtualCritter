9d5c3ff59428ee2e6a1b52db9888514c7e7d3eb3
helagrz <helena.grzelak2025@my.ntu.ac.uk>
2025-10-15 04:15:53 -0700

    Name

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ class critter(object):
     """A virtual pet"""
     def __init__(self):
         print("A new critter has been born")
+        self.name = ""
 
+    def __str__(self):
+        info = "Critter info:\n"
+        info += "   Name: " + self.name + "\n"
+        return info
 
+    def name_critter(self, name):
+        self.name = name
 
 c1 = critter()
-
+print("What name do you want to give your critter?")
+name = input()
+name = name.capitalize()
+c1.name_critter(name)
+print(c1)
