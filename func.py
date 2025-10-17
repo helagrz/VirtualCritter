b659c19801ac996f47092093ab63c51cd88b97d8
N1404763 <hela.grz8@gmail.com>
2025-10-17 09:58:51 -0800

    Selection basic

--- func.py
+++ func.py
@@ -2,6 +2,12 @@
 
 import random
 
+def welcome():
+    print("Welcome to the virtual critter program!")
+    print("Here you can take care of your pets")
+    print("Keep good care of them by feeding them playing with them and more!")
+
+
 def create_critter(c1):
     print("What name do you want to give your critter?")
     name = input()
--- main.py
+++ main.py
@@ -19,16 +19,36 @@ class critter(object):
         self.hunger = 5
 
     def __str__(self):
+        return self.info_c()
+        
+    def info_c(self):
         info = "Critter info:\n"
         info += "   Name: " + self.name + "\n"
         info += "   Personality: " + self.pers + "\n"
+        print(info)
         return info
+
+    def selection(self):
+        print("What do you want to do?")
+        print("""
+            1 - INFO
+            2 - CHECK
+            3 - FEED
+            4 - PLAY
+            5 - MORE
+              """)
+        x = int(input())
+        match x:
+            case 1:
+                print("You selected: INFO")
+                self.info_c()
+
 
     def name_critter(self, name):
         self.name = name
 
     def mood(self):
-        print(self.name"feels",self.__mood)
+        print(self.name,"feels",self.__mood)
 
     def feed(self):
         if(self.hunger >= 10):
@@ -43,3 +63,9 @@ class critter(object):
 
 c1 = critter()
 func.create_critter(c1)
+
+continue_pl = True
+print(c1)
+
+c1.selection()
+
